fd9e84e37d17e932231831294df7b9e4e8dad382
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-25 12:42:56 +0800

    feat(layer): introduce virtual method in TS.

--- js/layer.ts
+++ js/layer.ts
@@ -1,5 +1,9 @@
 import { Tensor } from "./tensor.js";
 
 export abstract class Layer {
+    loadParams?(): void;
+    
+    loadWeights?(): void;
+
     abstract forward(input: Tensor, output: Tensor): void; 
 }
