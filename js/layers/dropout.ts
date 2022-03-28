b2b639a06c28de47e9013ce67a7c53cfa060ed74
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-28 10:31:16 +0800

    feat(dropout): finish dropout layer with ratio param.

---
+++ js/layers/dropout.ts
@@ -0,0 +1,22 @@
+import { onnx } from "onnx-proto";
+import { Tensor, TensorBuilder } from "../tensor";
+
+export function handleDropout(inputs: Tensor[], attributes: onnx.AttributeProto[]): Tensor[] {
+    const data = inputs[0];
+    const ratio = attributes[0].f;
+    const scale = 1 / (1 -ratio)
+    let result = forward(data, scale);
+
+    // Mask is not needed in inference
+    return [result, new Tensor()];
+}
+
+export function forward(input: Tensor, scale: number): Tensor {
+    let result = TensorBuilder.withShape(input.shape);
+
+    for (let index = 0; index < input.data.length; index++) {
+        result.data[index] = result.data[index] * scale;
+    }
+
+    return result;
+}
