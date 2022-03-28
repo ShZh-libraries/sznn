b091a29bc071dab65f913cca4f9200dcaeb49540
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-28 09:12:53 +0800

    feat(relu): add normal relu layer.

--- js/layers/relu.ts
+++ js/layers/relu.ts
@@ -1,15 +1,33 @@
 import { onnx } from "onnx-proto";
 import { Tensor, TensorBuilder } from "../tensor";
 
-export function handleLeakyRelu(input: Tensor[], attribute: onnx.AttributeProto[]): Tensor[] {
-    const data = input[0];
-    const alpha = attribute[0].f;
-    let result = forward(data, alpha);
+export function handleRelu(inputs: Tensor[]): Tensor[] {
+    const data = inputs[0];
+    let result = forwardRelu(data);
 
     return [result];
 }
 
-export function forward(data: Tensor, alpha: number): Tensor {
+export function handleLeakyRelu(inputs: Tensor[], attributes: onnx.AttributeProto[]): Tensor[] {
+    const data = inputs[0];
+    const alpha = attributes[0].f;
+    let result = forwardLeakRelu(data, alpha);
+
+    return [result];
+}
+
+export function forwardRelu(data: Tensor): Tensor {
+    let result = TensorBuilder.withShape(data.shape);
+
+    for (let index = 0; index < data.data.length; index++) {
+        result.data[index] = data.data[index] > 0? data.data[index] : 0;
+    }
+
+    return result;
+}
+
+
+export function forwardLeakRelu(data: Tensor, alpha: number): Tensor {
     let result = TensorBuilder.withShape(data.shape);
 
     for (let index = 0; index < data.data.length; index++) {
--- test/relu.spec.ts
+++ test/relu.spec.ts
@@ -1,13 +1,21 @@
-import { forward } from "../js/layers/relu";
-import { Tensor, TensorBuilder } from "../js/tensor";
+import { forwardRelu, forwardLeakRelu} from "../js/layers/relu";
+import { TensorBuilder } from "../js/tensor";
 
-describe("Test JS backend for leaky relu layer", () => {
-    test("Test leaky relu correctness", () => {
+describe("Test JS backend for relu layer", () => {
+    test("Test relu", () => {
+        const input = TensorBuilder.withData([-1, 0, 1]);
+
+        const result = forwardRelu(input);
+        expect(result.shape).toEqual([3]);
+        expect(result.data).toEqual(new Float32Array([0, 0, 1]));
+    });
+
+    test("Test leaky relu", () => {
         const input = TensorBuilder.withData([-1, 0, 1]);
         const alpha = 0.5;
 
-        const result = forward(input, alpha);
+        const result = forwardLeakRelu(input, alpha);
         expect(result.shape).toEqual([3]);
         expect(result.data).toEqual(new Float32Array([-0.5, 0, 1]));
-    })
+    });
 });
