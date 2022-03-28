9c80c3816df8279e8eba8289caad940ac4f7dc74
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-28 19:24:56 +0800

    fix(softmax): downgrade to simple softmax layer.

--- js/layers/softmax.ts
+++ js/layers/softmax.ts
@@ -1,42 +1,31 @@
 import { Tensor, TensorBuilder } from "../tensor";
 
-// axis=-1 implementation
-// TODO: forward_inplace
+// The default axis is 1
 export function handleSoftmax(inputs: Tensor[]): Tensor[] {
     return [forward(inputs[0])];
 }
 
+// For 1 x C x W x H tensor:
 export function forward(input: Tensor): Tensor {
     const resultShape = input.shape.slice();
     const result = TensorBuilder.withShape(resultShape);
 
-    const dataChannelSize = input.shape[2] * input.shape[3];
-    const dataSize = input.shape[1] * dataChannelSize;
-
-    for (let n = 0; n < input.shape[0]; n++) {
-        for (let c = 0; c < input.shape[1]; c++) {
-            for (let y = 0; y < input.shape[2]; y++) {
-                let off = n * dataSize + c * dataChannelSize + y * input.shape[3];
-
-                let max = input.data[off];
-                for (let x = 0; x < input.shape[3]; x++) {
-                    if (max < input.data[off + x]) {
-                        max = input.data[off + x];
-                    }
-                }
-
-                let sum = 0;
-                for (let x = 0; x < input.shape[3]; x++) {
-                    // Partial inplace
-                    input.data[off + x] = Math.exp(input.data[off + x] - max);
-                    sum += input.data[off + x];
-                }
-
-                for (let x = 0; x < input.shape[3]; x++) {
-                    result.data[off + x] = input.data[off + x] / sum;
-                }
-            }
+    let max = input.data[0];
+    for (let c = 0; c < input.shape[1]; c++) {
+        if (max < input.data[c]) {
+            max = input.data[c];
         }
+    }
+
+    let sum = 0;
+    for (let c = 0; c < input.shape[1]; c++) {
+        // Partial inplace
+        input.data[c] = Math.exp(input.data[c] - max);
+        sum += input.data[c];
+    }
+
+    for (let c = 0; c < input.shape[1]; c++) {
+        result.data[c] = input.data[c] / sum;
     }
 
     return result;
--- test/softmax.spec.ts
+++ test/softmax.spec.ts
@@ -3,10 +3,10 @@ import { TensorBuilder } from "../js/tensor";
 
 describe("Test JS backend for softmax layer", () => {
     test("Test default softmax", () => {
-        const src = TensorBuilder.withData([[[[-1, 0, 1]]]]);
+        const src = TensorBuilder.withData([[-1, 0, 1]]);
         const result = forward(src);
 
-        expect(result.shape).toEqual([1, 1, 1, 3]);
+        expect(result.shape).toEqual([1, 3]);
         expect(result.data).toEqual(new Float32Array([0.09003057, 0.24472848, 0.66524094]));
     });
 });
