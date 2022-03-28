9041c24b4a473c9f5085639f38f3032be83d4169
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-28 10:52:43 +0800

    feat(pooling): add average pooling layer impl.

--- js/layers/pooling.ts
+++ js/layers/pooling.ts
@@ -1,14 +1,14 @@
 import { onnx } from "onnx-proto";
 import { Tensor, TensorBuilder } from "../tensor";
 
-export class MaxPoolAttr {
+export class PoolingAttr {
     kernelShape: number[] = [];
     pads: number[] = [0, 0, 0, 0];
     strides: number[] = [1, 1];
 }
 
-function handleAttribute(attributes: onnx.AttributeProto[]): MaxPoolAttr {
-    let result: MaxPoolAttr = new MaxPoolAttr();
+function handleAttribute(attributes: onnx.AttributeProto[]): PoolingAttr {
+    let result: PoolingAttr = new PoolingAttr();
     for (const attribute of attributes) {
         switch(attribute.name) {
             case "kernel_shape":
@@ -28,15 +28,21 @@ function handleAttribute(attributes: onnx.AttributeProto[]): MaxPoolAttr {
     return result;
 }
 
-export function handleMaxPool(input: Tensor[], attributes: onnx.AttributeProto[]): Tensor[] {
+export function handleMaxPool2D(inputs: Tensor[], attributes: onnx.AttributeProto[]): Tensor[] {
     const maxPoolingAttr = handleAttribute(attributes);
-    const result = forward(input[0], maxPoolingAttr);
+    const result = forwardMaxPool2D(inputs[0], maxPoolingAttr);
     
     return [result];
 }
 
-// 2D version
-export function forward(data: Tensor, maxPoolingAttr: MaxPoolAttr): Tensor {
+export function handleAvgPool2D(inputs: Tensor[], attributes: onnx.AttributeProto[]): Tensor[] {
+    const avgPoolingAttr = handleAttribute(attributes);
+    const result = forwardAvgPool2D(inputs[0], avgPoolingAttr);
+
+    return [result];
+}
+
+export function forwardMaxPool2D(data: Tensor, maxPoolingAttr: PoolingAttr): Tensor {
     // Calculate shape
     const maxY = data.shape[2] + maxPoolingAttr.pads[0] + maxPoolingAttr.pads[2] - maxPoolingAttr.kernelShape[0];
     const maxX = data.shape[3] + maxPoolingAttr.pads[1] + maxPoolingAttr.pads[3] - maxPoolingAttr.kernelShape[1];
@@ -73,6 +79,47 @@ export function forward(data: Tensor, maxPoolingAttr: MaxPoolAttr): Tensor {
                     }
 
                     result.data[resultIndex++] = maxValue;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+export function forwardAvgPool2D(data: Tensor, avgPoolingAttr: PoolingAttr): Tensor {
+    // Calculate shape
+    const maxY = data.shape[2] + avgPoolingAttr.pads[0] + avgPoolingAttr.pads[2] - avgPoolingAttr.kernelShape[0];
+    const maxX = data.shape[3] + avgPoolingAttr.pads[1] + avgPoolingAttr.pads[3] - avgPoolingAttr.kernelShape[1];
+    const resultHeight = Math.floor(maxY / avgPoolingAttr.strides[0]) + 1;
+    const resultWidth = Math.floor(maxX / avgPoolingAttr.strides[1]) + 1;
+
+    const kernelSize = avgPoolingAttr.kernelShape[0] * avgPoolingAttr.kernelShape[1];
+    const dataChannelSize = data.shape[2] * data.shape[3];
+    const dataSize = data.shape[1] * dataChannelSize;
+    
+    const result = TensorBuilder.withShape([data.shape[0], data.shape[1], resultHeight, resultWidth]);
+    let resultIndex = 0;
+    for (let n = 0; n < data.shape[0]; n++) {
+        for (let c = 0; c < data.shape[1]; c++) {
+            for (let y = 0; y <= maxY; y += avgPoolingAttr.strides[0]) {
+                for (let x = 0; x <= maxX; x += avgPoolingAttr.strides[1]) {
+                    const realY = y - avgPoolingAttr.pads[0];
+                    const realX = x - avgPoolingAttr.pads[1];
+
+                    let sum = 0;
+                    for (let ky = 0; ky < avgPoolingAttr.kernelShape[0]; ky++) {
+                        for (let kx = 0; kx < avgPoolingAttr.kernelShape[1]; kx++) {
+                            const dataY = realY + ky;
+                            const dataX = realX + kx;
+
+                            if (dataY >= 0 && dataY < data.shape[2] && dataX >= 0 && dataX < data.shape[3]) {
+                                sum += data.data[n * dataSize + c * dataChannelSize + dataY * data.shape[3] + dataX];
+                            }
+                        }
+                    }
+
+                    result.data[resultIndex++] = sum / kernelSize;
                 }
             }
         }
--- test/pooling.spec.ts
+++ test/pooling.spec.ts
@@ -1,29 +1,46 @@
-import { forward, MaxPoolAttr } from "../js/layers/pooling";
+import { forwardAvgPool2D, forwardMaxPool2D, PoolingAttr } from "../js/layers/pooling";
 import { TensorBuilder } from "../js/tensor";
 
-describe("Test JS backend for max pooling layer", () => {
-    test("Test normal max pooling layer", () => {
-        const src = TensorBuilder.withData([[
-            [
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9],
-            ],
-            [
-                [10, 11, 12],
-                [13, 14, 15],
-                [16, 17, 18]
-            ]
-        ]]);
-        const maxPoolingAttr = new MaxPoolAttr();
-        maxPoolingAttr.kernelShape = [2, 2];
-        maxPoolingAttr.strides = [1, 1];
+describe("Test JS backend for pooling layer", () => {
+    const src = TensorBuilder.withData([[
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ],
+        [
+            [10, 11, 12],
+            [13, 14, 15],
+            [16, 17, 18]
+        ]
+    ]]);
 
-        const result = forward(src, maxPoolingAttr);
+    describe("Test avg pooling layer", () => {
+        test("Test normal avg pooling layer", () => {
+            const avgPoolingAttr = new PoolingAttr();
+            avgPoolingAttr.kernelShape = [2, 2];
+            avgPoolingAttr.strides = [1, 1];
+    
+            const result = forwardAvgPool2D(src, avgPoolingAttr);
+            expect(result.shape).toEqual([1, 2, 2, 2]);
+            expect(result.data).toEqual(new Float32Array([
+                3, 4, 6, 7, 12, 13, 15, 16
+            ]));
+        })
+    })
 
-        expect(result.shape).toEqual([1, 2, 2, 2]);
-        expect(result.data).toEqual(new Float32Array([
-            5, 6, 8, 9, 14, 15, 17, 18
-        ]))
+    describe("Test max pooling layer", () => {
+        test("Test normal max pooling layer", () => {
+            const maxPoolingAttr = new PoolingAttr();
+            maxPoolingAttr.kernelShape = [2, 2];
+            maxPoolingAttr.strides = [1, 1];
+    
+            const result = forwardMaxPool2D(src, maxPoolingAttr);
+    
+            expect(result.shape).toEqual([1, 2, 2, 2]);
+            expect(result.data).toEqual(new Float32Array([
+                5, 6, 8, 9, 14, 15, 17, 18
+            ]))
+        });
     });
 });
