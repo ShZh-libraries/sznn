8d2500a178bef88d59b5ada69f2b32fbf6bd8f57
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-29 21:25:15 +0800

    layer(gather): finish simple gather layer.

---
+++ js/layers/gather.ts
@@ -0,0 +1,23 @@
+import { Tensor, TensorBuilder } from "../tensor";
+
+// With axis = 0 and inputs[2] only has 1 dim
+export function handleGather(inputs: Tensor[]): Tensor[] {
+    const output = forward(inputs[0], inputs[1]);
+
+    return [output];
+}
+
+export function forward(input: Tensor, indices: Tensor): Tensor {
+    let outputShape = input.shape.slice();
+    outputShape[indices.data[0]] = 1;
+    
+    let output = TensorBuilder.withShape(outputShape);
+
+    let outputLength = outputShape.reduceRight((x, y) => x * y);
+    const channelSize = input.shape[2] * input.shape[3];
+    for (let index = 0; index < outputLength; index++) {
+        output.data[index] = input.data[channelSize * indices.data[0] + index];
+    }
+
+    return output;
+}
