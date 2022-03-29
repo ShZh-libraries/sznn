245a8d15ae6cec920474753807c2b72decb55c7d
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-30 00:07:08 +0800

    test(slice): simple test for slice layer.

---
+++ test/slice.spec.ts
@@ -0,0 +1,17 @@
+import { forward, SliceAttr } from "../js/layers/slice";
+import { TensorBuilder } from "../js/tensor";
+
+describe("Test JS backend for slice layer", () => {
+    test("Test simple slice layer", () => {
+        const input = TensorBuilder.withData([1, 3, 255, 255]);
+        let sliceAttr = new SliceAttr();
+        sliceAttr.axes = [0];
+        sliceAttr.starts = [2];
+        sliceAttr.ends = [4];
+
+        const output = forward(input, sliceAttr);
+
+        expect(output.shape).toEqual([2]);
+        expect(output.data).toEqual(new Float32Array([255, 255]));
+    });
+});
