ae95028cb8b6498d7999d25276a1f37658a2e5c1
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-29 19:23:15 +0800

    feat(instancenorm): finish instance norm layer.

---
+++ js/layers/instancenorm.ts
@@ -0,0 +1,38 @@
+import { onnx } from "onnx-proto";
+import { Tensor } from "../tensor";
+
+export function handleInstanceNorm(inputs: Tensor[], attributes: onnx.AttributeProto[]): Tensor[] {
+    const epsilon = attributes[0].f;
+    const result = forward(inputs[0], inputs[1], inputs[2], epsilon);
+
+    return [result];
+}
+
+// Not consider number dim
+export function forward(input: Tensor, weight: Tensor, bias: Tensor, epsilon: number): Tensor {
+    let output = input.copy();
+    const channelSize = output.shape[2] * output.shape[3];
+
+    for (let c = 0; c < output.shape[1]; c++) {
+        // Calculate mean
+        let mean = 0;
+        for (let i = 0; i < channelSize; i++) {
+            mean += input.data[c * channelSize + i];
+        }
+        mean /= channelSize;
+        // Calculate variance
+        let variance = 0;
+        for (let i = 0; i < channelSize; i++) {
+            let stdVar = (input.data[c * channelSize + i] - mean);
+            variance += stdVar * stdVar;
+        }
+        variance /= channelSize;
+        // Normalization
+        for (let i = 0; i < channelSize; i++) {
+            output.data[c * channelSize + i] = 
+                weight.data[c] * (input.data[c * channelSize + i] - mean) / Math.sqrt(variance + epsilon) + bias.data[c];
+        }
+    }
+
+    return output;
+}
---
+++ test/instancenorm.spec.ts
@@ -0,0 +1,17 @@
+import { forward } from "../js/layers/instancenorm";
+import { TensorBuilder } from "../js/tensor";
+
+describe("Test JS backends for instance normalization", () => {
+    test("Test instance normalization", () => {
+        const input = TensorBuilder.withData([[[[-1, 0, 1]], [[2, 3, 4]]]]);
+        const weight = TensorBuilder.withData([1, 1.5]);
+        const bias = TensorBuilder.withData([0, 1]);
+
+        const result = forward(input, weight, bias, 0);
+        expect(result.shape).toEqual([1, 2, 1, 3]);
+        expect(result.data).toEqual(new Float32Array([
+            -1.224744871391589, 0, 1.224744871391589,
+            -0.8371173070873834, 1, 2.8371173070873834,
+        ]));
+    });
+});
