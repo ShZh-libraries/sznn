d79c06654f828ca02121999224e444fe7a95ca97
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-03-29 20:38:55 +0800

    feat(binaryop): finish binary op layer.

---
+++ js/layers/binaryop.ts
@@ -0,0 +1,76 @@
+import { Tensor, TensorBuilder } from "../tensor";
+
+type BinaryFunc = (x: number, y: number) => number;
+
+export function handleBinaryOp(inputs: Tensor[], op: BinaryFunc): Tensor[] {
+    const a = inputs[0];
+    const b = inputs[1];
+    const output = forward(a, b, op);
+
+    return [output];
+}
+
+export function forward(a: Tensor, b: Tensor, op: BinaryFunc): Tensor {
+    const outputShape = getBroadcastShape(a.shape, b.shape);
+    const output = TensorBuilder.withShape(outputShape);
+
+    const aBroadcastDim = getBroadcastDims(a.shape, outputShape);
+    const bBroadcastDim = getBroadcastDims(b.shape, outputShape);
+
+    if (aBroadcastDim.length + bBroadcastDim.length == 0) {
+        for (let i = 0; i < output.data.length; i++) {
+            output.data[i] = op(a.data[i], b.data[i]);
+        }
+    } else {
+        for (let i = 0; i < output.data.length; i++) {
+            const outputLoc = output.getLoc(i);
+
+            const aLoc = outputLoc.slice(-a.ndim);
+            aBroadcastDim.forEach(dim => aLoc[dim] = 0);
+            const aIndex = a.atLoc(aLoc);
+
+            const bLoc = outputLoc.slice(-b.ndim);
+            bBroadcastDim.forEach(dim => bLoc[dim] = 0);
+            const bIndex = b.atLoc(bLoc);
+
+            output.data[i] = op(a.data[aIndex], b.data[bIndex]);
+        }
+    }
+
+    return output;
+}
+
+function getBroadcastShape(shape1: number[], shape2: number[]): number[] {
+    let result = [];
+    let resultLength = shape1.length > shape2.length? shape1.length : shape2.length;
+
+    for (let index = 0; index < resultLength; index++) {
+        let a = shape1[shape1.length - 1 - index]? shape1[shape1.length - 1 - index] : 1;
+        let b = shape2[shape2.length - 1 - index]? shape2[shape2.length - 1 - index] : 1;
+
+        if (a == 1) {
+            result.unshift(b);
+        } else if (b == 1) {
+            result.unshift(a);
+        } else if (a == b) {
+            result.unshift(a);
+        } else {
+            throw new Error("Cannot broadcast!!");
+        }
+    }
+
+    return result;
+}
+
+function getBroadcastDims(shape: number[], resultShape: number[]): number[] {
+  const result: number[] = [];
+  for (let i = 0; i < shape.length; i++) {
+    const dim = shape.length - 1 - i;
+    const a = shape[dim] || 1;
+    const b = resultShape[dim] || 1;
+    if (b > 1 && a === 1) {
+      result.unshift(dim);
+    }
+  }
+  return result;
+}
---
+++ test/binaryop.spec.ts
@@ -0,0 +1,22 @@
+import { forward } from "../js/layers/binaryop";
+import { TensorBuilder } from "../js/tensor";
+
+describe("Test JS backends for binary op", () => {
+    test("Test multiplication", () => {
+        const a = TensorBuilder.withData([[1, 2], [3, 4]]);
+        const b = TensorBuilder.withData([[5, 6], [7, 8]]);
+
+        const result = forward(a, b, (x, y) => x * y);
+        expect(result.shape).toEqual([2, 2]);
+        expect(result.data).toEqual(new Float32Array([5, 12, 21, 32]));
+    });
+
+    test("Test broadcast adding", () => {
+        const a = TensorBuilder.withData([[1, 2], [3, 4]]);
+        const b = TensorBuilder.withData([2]);
+
+        const result = forward(a, b, (x, y) => x + y);
+        expect(result.shape).toEqual([2, 2]);
+        expect(result.data).toEqual(new Float32Array([3, 4, 5, 6]));
+    })
+});
