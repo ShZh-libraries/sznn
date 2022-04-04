9cc14ea3bd617ae22679f9619c0ce650fc3630ba
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-04 16:24:17 +0800

    feat(wasm): finish reshape layer.

--- wasm/src/rs/src/layers/mod.rs
+++ wasm/src/rs/src/layers/mod.rs
@@ -2,3 +2,4 @@ pub mod abs;
 pub mod batchnorm;
 pub mod dropout;
 pub mod relu;
+pub mod reshape;
---
+++ wasm/src/rs/src/layers/reshape.rs
@@ -0,0 +1,26 @@
+use crate::tensor::Tensor;
+
+pub fn forward(input: &Tensor, shape: &mut Vec<isize>) -> Tensor {
+    // Deal with zero
+    let input_shape = input.get_shape();
+    for index in 0..shape.len() {
+        if shape[index] == 0 {
+            shape[index] = input_shape[index] as isize;
+        }
+    }
+
+    // Deal with negative numbers
+    let negative_idx = shape.iter().position(|&x| x == -1);
+    if let Some(idx) = negative_idx {
+        let remain_len = shape.iter().fold(1, |res, val| res * val) as usize;
+        let remain_size = input.length / remain_len;
+        shape[idx] = remain_size as isize;
+    }
+
+    let mut output = Tensor::new();
+    output.data = input.data.clone();
+    let shape = shape.iter().map(|&x| x as usize).collect();
+    output.set_vec_shape(shape);
+
+    output
+}
