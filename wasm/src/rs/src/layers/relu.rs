fe1846dee1ac45aa0a0591bc4bd62c59a27d8a9b
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-04 18:01:02 +0800

    feat(wasm): add vector of integers to relu layer.

--- wasm/src/rs/src/layers/relu.rs
+++ wasm/src/rs/src/layers/relu.rs
@@ -7,6 +7,27 @@ pub fn forward_relu(input: &Tensor) -> Tensor {
     let mut output = Tensor::new();
     output.set_vec_shape(input.get_shape());
     output.data = match &input.data {
+        TensorDataType::Int8(arr) => {
+            let out = arr
+                .iter()
+                .map(|&x| if x > 0 { x } else { 0 })
+                .collect::<Vec<_>>();
+            TensorDataType::Int8(out)
+        },
+        TensorDataType::Int16(arr) => {
+            let out = arr
+                .iter()
+                .map(|&x| if x > 0 { x } else { 0 })
+                .collect::<Vec<_>>();
+            TensorDataType::Int16(out)
+        },
+        TensorDataType::Int32(arr) => {
+            let out = arr
+                .iter()
+                .map(|&x| if x > 0 { x } else { 0 })
+                .collect::<Vec<_>>();
+            TensorDataType::Int32(out)
+        },
         TensorDataType::Float32(arr) => {
             let out = arr
                 .iter()
