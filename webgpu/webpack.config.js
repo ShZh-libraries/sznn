53dabba7c7830cfdbef64e3695aa6095fc04886b
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-07 23:28:08 +0800

    build(wgpu): add web gpu backend's scaffold.

---
+++ webgpu/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = [
+  {
+    mode: "development",
+    devtool: "source-map",
+    entry: path.join(__dirname, "src", "index.ts"),
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    output: {
+      filename: "index.js",
+      path: path.join(__dirname, "dist"),
+      library: "sznn_webgpu",
+      libraryTarget: "umd"
+    },
+  },
+];
