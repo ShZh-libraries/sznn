0651f3739fedf88e83f081f7633b066da66c0a61
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-10 00:55:00 +0800

    feat(webgpu): finish leaky relu layer.

--- webgpu/src/layers/relu.ts
+++ webgpu/src/layers/relu.ts
@@ -5,14 +5,48 @@ import { DType, Tensor, TensorBuilder } from "../tensor";
 export async function handleRelu(input: Tensor, device: GPUDevice): Promise<Tensor> {
     let output = TensorBuilder.withShape(input.shape);
 
-    const len = output.getLength();
-    const gpuMetaBuffer = setGPUReadBuffer(new Uint32Array([len]), DType.uint32, device);
     const gpuInputBuffer = input.setInputGPUBuffer(device);
     const gpuOutputBuffer = output.setOutputGPUBuffer(device);
+    const len = output.getLength();
+    const gpuMetaBuffer = setGPUReadBuffer(new Uint32Array([len]), DType.uint32, device);
+    const gpuAlphaBuffer = setGPUReadBuffer(new Float32Array([1]), DType.float32, device);
 
     const computePipeline = loadWGSL(relu, device, "relu");
 
-    const bindGroup = createBindGroup(computePipeline, [gpuInputBuffer, gpuOutputBuffer, gpuMetaBuffer], device);
+    const bindGroup = createBindGroup(computePipeline, [
+        gpuInputBuffer, 
+        gpuOutputBuffer, 
+        gpuMetaBuffer,
+        gpuAlphaBuffer,
+    ], device);
+
+    const commandEncoder = getCommandEncoder(computePipeline, bindGroup, [1], device);
+
+    const resultBuffer = await getResult(commandEncoder, gpuOutputBuffer, output.data.byteLength, device);
+    const resultArray = new Float32Array(resultBuffer);
+
+    output.data = resultArray;
+
+    return output;
+}
+
+export async function handleLeakyRelu(input: Tensor, alpha: number, device: GPUDevice): Promise<Tensor> {
+    let output = TensorBuilder.withShape(input.shape);
+
+    const gpuInputBuffer = input.setInputGPUBuffer(device);
+    const gpuOutputBuffer = output.setOutputGPUBuffer(device);
+    const len = output.getLength();
+    const gpuMetaBuffer = setGPUReadBuffer(new Uint32Array([len]), DType.uint32, device);
+    const gpuAlphaBuffer = setGPUReadBuffer(new Float32Array([alpha]), DType.float32, device);
+    
+    const computePipeline = loadWGSL(relu, device, "relu");
+
+    const bindGroup = createBindGroup(computePipeline, [
+        gpuInputBuffer, 
+        gpuOutputBuffer, 
+        gpuMetaBuffer,
+        gpuAlphaBuffer,
+    ], device);
 
     const commandEncoder = getCommandEncoder(computePipeline, bindGroup, [1], device);
 
--- webgpu/src/layers/wgsl/relu.wgsl
+++ webgpu/src/layers/wgsl/relu.wgsl
@@ -1,6 +1,7 @@
 @group(0) @binding(0) var<storage, read> input: array<f32>;
 @group(0) @binding(1) var<storage, write> output: array<f32>;
-@group(0) @binding(2) var<storage, read> len : u32;
+@group(0) @binding(2) var<storage, read> len: u32;
+@group(0) @binding(3) var<storage, read> alpha: f32;
 
 let workgroup_size_x = 256;
 
@@ -8,6 +9,6 @@ let workgroup_size_x = 256;
 @workgroup_size(workgroup_size_x)
 fn relu(@builtin(global_invocation_id) global_id : vec3<u32>) {
     for (var i: u32 = global_id.x; i < len; i += u32(workgroup_size_x)) {
-        output[i] = max(input[i], 0.);
+        output[i] = max(input[i], 0.) * alpha;
     }
 }
