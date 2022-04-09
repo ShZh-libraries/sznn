d461ecc698696dc0586e684cd11a2104b2363c74
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-10 00:36:06 +0800

    feat(webgpu): finish avg pooling layer.

--- webgpu/src/layers/pooling.ts
+++ webgpu/src/layers/pooling.ts
@@ -54,68 +54,59 @@ export async function handleMaxPool2D(input: Tensor, attr: PoolingAttr, device: 
 
   return output;
 }
+
+export async function handleAvgPool2D(input: Tensor, attr: PoolingAttr, device: GPUDevice): Promise<Tensor> {
+  // Padding first
+  const paddingAttr = attr.getPaddingAttr();
+  let paddingTensor: Tensor;
+  if (!paddingAttr.pads.every(x => x == 0)) {
+    paddingTensor = await handlePadding(input, paddingAttr, device);
+  } else {
+    paddingTensor = input;
+  }
   
-  // export function handleAvgPool2D(input: Tensor, attr: PoolingAttr): Tensor {
-  //   const kernelSize = attr.kernelShape[0] * attr.kernelShape[1];
-  //   // Calculate shape
-  //   const maxY =
-  //     input.shape[2] + attr.pads[0] + attr.pads[2] - attr.kernelShape[0];
-  //   const maxX =
-  //     input.shape[3] + attr.pads[1] + attr.pads[3] - attr.kernelShape[1];
-  //   const outputHeight = Math.floor(maxY / attr.strides[0]) + 1;
-  //   const outputWidth = Math.floor(maxX / attr.strides[1]) + 1;
-  //   const output = TensorBuilder.withShape([
-  //     input.shape[0],
-  //     input.shape[1],
-  //     outputHeight,
-  //     outputWidth,
-  //   ]);
+  const maxY = paddingTensor.shape[2] - attr.kernelShape[0];
+  const maxX = paddingTensor.shape[3] - attr.kernelShape[1];
+  const outputHeight = Math.floor(maxY / attr.strides[0]) + 1;
+  const outputWidth = Math.floor(maxX / attr.strides[1]) + 1;
+  const output = TensorBuilder.withShape([
+    paddingTensor.shape[0],
+    paddingTensor.shape[1],
+    outputHeight,
+    outputWidth,
+  ]);
+
+  const gpuInputBuffer = paddingTensor.setInputGPUBuffer(device);
+  const gpuInShapeBuffer = setGPUReadBuffer(new Uint32Array(paddingTensor.shape), DType.uint32, device);
+  const gpuOutputBuffer = output.setOutputGPUBuffer(device);
+  const gpuOutShapeBuffer = setGPUReadBuffer(new Uint32Array(output.shape), DType.uint32, device);
+  const gpuAttrBuffer = setGPUReadBuffer(new Uint32Array([
+      attr.kernelShape[0], attr.kernelShape[1],
+      attr.strides[0], attr.strides[1]
+  ]), DType.uint32, device);
+
+  const computePipeline = loadWGSL(pooling, device, "avg_pool");
+  const bindGroup = createBindGroup(computePipeline, [
+      gpuInputBuffer, gpuInShapeBuffer, 
+      gpuOutputBuffer, gpuOutShapeBuffer,
+      gpuAttrBuffer
+  ], device);
+  // TODO
+  const commandEncoder = getCommandEncoder(
+    computePipeline, 
+    bindGroup, 
+    [Math.ceil(output.shape[3] / 8), Math.ceil(output.shape[2] / 8), Math.ceil(output.shape[1] / 4)], 
+    device
+  );
+
+  const resultBuffer = await getResult(commandEncoder, gpuOutputBuffer, output.data.byteLength, device);
+  const resultArray = new Float32Array(resultBuffer);
+  output.data = resultArray;
+
+  return output;
+}
   
-  //   const inputChannelSize = input.shape[2] * input.shape[3];
-  //   const inputSize = input.shape[1] * inputChannelSize;
-  
-  //   let outputIndex = 0;
-  //   for (let n = 0; n < input.shape[0]; n++) {
-  //     for (let c = 0; c < input.shape[1]; c++) {
-  //       for (
-  //         let y = -attr.pads[0];
-  //         y <= maxY - attr.pads[0];
-  //         y += attr.strides[0]
-  //       ) {
-  //         for (
-  //           let x = -attr.pads[1];
-  //           x <= maxX - attr.pads[1];
-  //           x += attr.strides[1]
-  //         ) {
-  //           let sum = 0;
-  //           for (let ky = 0; ky < attr.kernelShape[0]; ky++) {
-  //             for (let kx = 0; kx < attr.kernelShape[1]; kx++) {
-  //               const cy = y + ky;
-  //               const cx = x + kx;
-  //               if (
-  //                 cy >= 0 &&
-  //                 cy < input.shape[2] &&
-  //                 cx >= 0 &&
-  //                 cx < input.shape[3]
-  //               ) {
-  //                 const curIdx =
-  //                   n * inputSize +
-  //                   c * inputChannelSize +
-  //                   cy * input.shape[3] +
-  //                   cx;
-  //                 sum += input.data[curIdx];
-  //               }
-  //             }
-  //           }
-  //           output.data[outputIndex++] = sum / kernelSize;
-  //         }
-  //       }
-  //     }
-  //   }
-  
-  //   return output;
-  // }
-  
+
   // export function handleGlobalAvgPool(input: Tensor): Tensor {
   //   const globalAvgPoolingAttr = new PoolingAttr();
   //   globalAvgPoolingAttr.kernelShape = [input.shape[2], input.shape[3]];
--- webgpu/src/layers/wgsl/pooling.wgsl
+++ webgpu/src/layers/wgsl/pooling.wgsl
@@ -46,3 +46,35 @@ fn max_pool(@builtin(global_invocation_id) global_id: vec3<u32>) {
     let out_idx = out_c * out_shape[2] * out_shape[3] + out_y * out_shape[3] + out_x;
     output[out_idx] = max_val;
 }
+
+@stage(compute)
+@workgroup_size(workgroup_size_x, workgroup_size_y, workgroup_size_z)
+fn avg_pool(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    let out_x = global_id.x;
+    let out_y = global_id.y;
+    let out_c = global_id.z;
+
+    if (out_x >= out_shape[3] || out_y >= out_shape[2] || out_c >= out_shape[1]) {
+        return;
+    }
+
+    let in_start_x = out_x * attr.stride_x;
+    let in_start_y = out_y * attr.stride_y;
+    let in_c = out_c;
+
+    var sum = 0.;
+    for (var ky = 0u; ky < attr.kernel_height; ky++) {
+        for (var kx = 0u; kx < attr.kernel_width; kx++) {
+            let in_x = in_start_x + kx;
+            let in_y = in_start_y + ky;
+
+            let in_idx = in_c * in_shape[2] * in_shape[3] + in_y * in_shape[3] + in_x;
+            let cur_val = input[in_idx];
+            sum += cur_val;
+        }
+    }
+
+    let out_idx = out_c * out_shape[2] * out_shape[3] + out_y * out_shape[3] + out_x;
+    output[out_idx] = sum / f32(attr.kernel_height * attr.kernel_width);
+}
+
