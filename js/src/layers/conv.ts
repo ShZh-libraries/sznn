acff5a419628f7ab3a75517cc5f87ae00a82d298
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-23 21:53:39 +0800

    refactor(js): iterate output to calculate result.

--- js/src/layers/conv.ts
+++ js/src/layers/conv.ts
@@ -26,16 +26,11 @@ export function handleConv(
   let resultIndex = 0;
   for (let n = 0; n < outputSize; n++) {
     for (let c = 0; c < outputChannel; c++) {
-      for (
-        let y = -attr.pads[0];
-        y <= maxY - attr.pads[0];
-        y += attr.strides[1]
-      ) {
-        for (
-          let x = -attr.pads[1];
-          x <= maxX - attr.pads[1];
-          x += attr.strides[0]
-        ) {
+      for (let out_y = 0; out_y < outputHeight; out_y++) {
+        for (let out_x = 0; out_x < outputWidth; out_x++) {
+          const y = out_y * attr.strides[0] - attr.pads[0];
+          const x = out_x * attr.strides[1] - attr.pads[1];
+
           let sum = 0;
           for (let ky = 0; ky < attr.kernelShape[1]; ky++) {
             for (let kx = 0; kx < attr.kernelShape[0]; kx++) {
@@ -77,53 +72,3 @@ export function handleConv(
 
   return output;
 }
-
-// export function forward(data: Tensor, weight: Tensor, convAttr: ConvAttr): Tensor[] {
-//     // Calculate shape
-//     const resultSize = data.shape[0];
-//     const resultChannel = weight.shape[0];
-//     const kernelWidth = weight.shape[2];
-//     const kernelHeight = weight.shape[3];
-
-//     const resultWidth = Math.floor(data.shape[2] + convAttr.pads[0] + convAttr.pads[2] - kernelWidth) / convAttr.strides[0] + 1;
-//     const resultHeight = Math.floor(data.shape[3] + convAttr.pads[1] + convAttr.pads[3] - kernelHeight) / convAttr.strides[1] + 1;
-//     const resultShape = [resultSize, resultChannel, resultWidth, resultHeight];
-//     const result = TensorBuilder.withShape(resultShape);
-
-//     // Do convolution
-//     const kernelSize = kernelWidth * kernelHeight;
-//     for (let y = 0; y < resultHeight; y++) {
-//         const tempTensor = im2row(data, convAttr, y * convAttr.strides[1]);
-//         for (let x = 0; x < resultWidth; x++) {
-//             for (let c = 0; c < resultChannel; c++) {
-//                 let sum = 0;
-//                 for (let i = 0; i < kernelSize; i++) {
-//                     sum += weight.data[c * kernelSize + i] as number
-//                 }
-//             }
-//         }
-//     }
-
-//     return [result];
-// }
-
-// export function im2row(data: Tensor, convAttr: ConvAttr, y: number): Tensor {
-//     const channel = data.shape[1];
-//     const width = data.shape[2];
-
-//     for (let i = 0; i < channel; i++) {
-//         for (let j = 0; j < convAttr.kernelShape[1]; j++) {
-//             for (let x = 0; x < width; x += convAttr.strides[0]) {
-//                 for (let k = 0; k < convAttr.kernelShape[0]; x++) {
-//                     const srcX = x - convAttr.pads[0] + k;
-//                     const srcY = y - convAttr.pads[1] + j;
-
-//                     let target = 0;
-//                     if (srcX > 0 && srcY > 0 && srcX < data.shape[2] && srcY < data.shape[3]) {
-//                         target = data.data[1] as number;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
