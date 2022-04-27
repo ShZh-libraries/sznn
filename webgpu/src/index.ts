48193cc801f747425d0a25687baf0f25f45d452d
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-27 14:07:17 +0800

    test(webgpu): deal with some details in test environment.

--- webgpu/src/index.ts
+++ webgpu/src/index.ts
@@ -2,16 +2,3 @@
 
 export { loadModel, Model } from "./model";
 export { Tensor, TensorBuilder } from "./tensor";
-
-// import { ConvAttr } from "../../core/attr/conv";
-// import { getGPUDevice } from "./gpu";
-// import { handleConv } from "./layers/conv";
-// import { TensorBuilder } from "./tensor";
-
-// (async () => {
-//   const device = await getGPUDevice();
-//   const input = TensorBuilder.withData([-1, 0, 1]);
-
-//   const output = await handleLeakyRelu(input, 0.1, device!);
-//   console.log(output);
-// })()
