fe569bf011b91b043c16805f5b8fd721ec878364
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-04-29 15:34:07 +0800

    test(webgpu): finish instancenorm layer's unit test.

---
+++ webgpu/test/instancenorm.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from "chai";
+
+import { TensorBuilder } from "../src";
+import { getGPUDevice } from "../src/gpu";
+import { handleInstanceNorm } from "../src/layers/instancenorm";
+
+describe("Test instancenorm layer of WebGPU backend", () => {
+    it("Test normal relu layer", async () => {
+        const device = await getGPUDevice();
+        const input = TensorBuilder.withData([[[[-1, 0, 1]], [[2, 3, 4]]]]);
+        const weight = TensorBuilder.withData([1, 1.5]);
+        const bias = TensorBuilder.withData([0, 1]);
+        const output = await handleInstanceNorm(input, weight, bias, 0, device!);
+
+        console.log(output.data);
+
+        expect(output.shape).deep.equal([1, 2, 1, 3]);
+        expect(output.data).deep.equal(new Float32Array([
+            -1.2247449159622192,
+            0, 
+            1.2247449159622192, 
+            -0.8371176719665527, 
+            0.9999998211860657,
+            2.8371171951293945,
+        ]));
+    });
+});
