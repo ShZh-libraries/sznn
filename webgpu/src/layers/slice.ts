be5254c26577c95823d8f7959c8c50f51d5adad4
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-05 16:20:46 +0800

    feat(webgpu): finish slice operation.

---
+++ webgpu/src/layers/slice.ts
@@ -0,0 +1,16 @@
+import { Tensor, TensorBuilder } from "../tensor";
+import { SliceAttr } from "../../../core/attr/slice";
+
+export function handleSlice(input: Tensor, attr: SliceAttr): Tensor {
+    if (attr.axes.length == 1 && attr.axes[0] == 0) {
+        const outputShape = attr.ends[0] - attr.starts[0];
+        const output = TensorBuilder.withShape([outputShape]);
+        for (let i = attr.starts[0]; i < attr.ends[0]; i++) {
+            output.data[i - attr.starts[0]] = input.data[i];
+        }
+
+        return output;
+    }
+
+    return new Tensor();
+}
