e0d91f9f9d2bc453816af824db54b495abd9cb80
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-08 00:26:42 +0800

    test(wasm): move abs test code to separate folder.

--- wasm/src/rs/src/layers/abs.rs
+++ wasm/src/rs/src/layers/abs.rs
@@ -33,20 +33,3 @@ pub fn handle_abs(input: &Tensor) -> Tensor {
 
     Tensor::new(out_data, out_shape)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_abs_forward() {
-        let input = Tensor::new(DTypes::I32(vec![1, -2, -3, 4]), vec![2, 2]);
-
-        let output = handle_abs(&input);
-
-        assert_eq!(output.get_length(), 4);
-        assert_eq!(output.get_dim(), 2);
-        assert_eq!(output.get_shape(), vec![2, 2]);
-        assert_eq!(*output.get_data(), DTypes::I32(vec![1, 2, 3, 4]));
-    }
-}
