69df8fe59395a96ce7e024c904156e259c80ab72
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-10 13:19:54 +0800

    fix(wasm): remove test code in wasm end.
    It is impossbile for `wasm-bindgen-test` to test multithreading code.
    So we decided to put the test part in JS end.

--- wasm/src/rs/src/layers/abs.rs
+++ wasm/src/rs/src/layers/abs.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use wasm_bindgen::prelude::*;
 
 use crate::tensor::{DTypes, Tensor};
@@ -7,23 +8,23 @@ pub fn handle_abs(input: &Tensor) -> Tensor {
     let out_shape = input.get_shape();
     let out_data = match &input.get_data() {
         DTypes::I8(arr) => {
-            let out = arr.iter().map(|x| x.abs()).collect::<Vec<_>>();
+            let out = arr.par_iter().map(|x| x.abs()).collect::<Vec<_>>();
             DTypes::I8(out)
         }
         DTypes::I16(arr) => {
-            let out = arr.iter().map(|x| x.abs()).collect::<Vec<_>>();
+            let out = arr.par_iter().map(|x| x.abs()).collect::<Vec<_>>();
             DTypes::I16(out)
         }
         DTypes::I32(arr) => {
-            let out = arr.iter().map(|x| x.abs()).collect::<Vec<_>>();
+            let out = arr.par_iter().map(|x| x.abs()).collect::<Vec<_>>();
             DTypes::I32(out)
         }
         DTypes::F32(arr) => {
-            let out = arr.iter().map(|x| x.abs()).collect::<Vec<_>>();
+            let out = arr.par_iter().map(|x| x.abs()).collect::<Vec<_>>();
             DTypes::F32(out)
         }
         DTypes::F64(arr) => {
-            let out = arr.iter().map(|x| x.abs()).collect::<Vec<_>>();
+            let out = arr.par_iter().map(|x| x.abs()).collect::<Vec<_>>();
             DTypes::F64(out)
         }
         _ => {
