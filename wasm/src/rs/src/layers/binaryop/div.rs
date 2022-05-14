0fbe2edd23aa21f90f0f8537052faf075824b933
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-14 21:08:26 +0800

    refactor(wasm): refactor all binary op by macro.

--- wasm/src/rs/src/layers/binaryop/div.rs
+++ wasm/src/rs/src/layers/binaryop/div.rs
@@ -1,382 +1,52 @@
+use paste::paste;
 use rayon::prelude::*;
 use wasm_bindgen::prelude::*;
 
 use crate::{DTypes, Tensor};
-use std::{arch::wasm32::*, sync::Mutex};
+use std::{arch::wasm32::*, sync::Mutex, mem};
 
-use super::{get_broadcast_shape, get_broadcast_dims, idx_to_loc, get_broadcast_loc, loc_to_idx};
+use super::*;
 
-#[wasm_bindgen(js_name = handleDiv)]
-pub fn handle_div(a: &Tensor, b: &Tensor) -> Tensor {
-    let (a_shape, b_shape) = (a.get_shape(), b.get_shape());
-    let out_shape = get_broadcast_shape(&a_shape, &b_shape);
-    let (a_broadcast_dim, b_broadcast_dim) = (get_broadcast_dims(&a_shape, &out_shape), get_broadcast_dims(&b_shape, &out_shape));
-
-    if a_broadcast_dim.len() + b_broadcast_dim.len() == 0 {
-        handle_same_shape(a, b, out_shape)
-    } else {
-        handle_broadcast(a, b, &a_broadcast_dim, &b_broadcast_dim, out_shape)
-    }
-}
-
-fn handle_same_shape(a: &Tensor, b: &Tensor, out_shape: Vec<usize>) -> Tensor {
-    let len = out_shape.iter().fold(1, |res, val| res * val);
-
+fn div_same_shape(a: &Tensor, b: &Tensor, out_shape: Vec<usize>) -> Tensor {
     let out_data = match &a.get_data() {
         DTypes::I8(a) => {
-            let b = if let DTypes::I8(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out: Vec<i8> = vec![0; len];
-            a.par_iter()
-                .zip(b.par_iter())
-                .zip(out.par_iter_mut())
-                .for_each(|((a, b), dst)| {
-                    *dst = a / b;
-                });
-            DTypes::I8(out)
+            let b = extract_data!(b, DTypes::I8);
+            DTypes::I8(par!(a, b, i8))
         }
         DTypes::I16(a) => {
-            let b = if let DTypes::I16(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out: Vec<i16> = vec![0; len];
-            a.par_iter()
-                .zip(b.par_iter())
-                .zip(out.par_iter_mut())
-                .for_each(|((a, b), dst)| {
-                    *dst = a / b;
-                });
-            DTypes::I16(out)
+            let b = extract_data!(b, DTypes::I16);
+            DTypes::I16(par!(a, b, i16))
         },
         DTypes::I32(a) => {
-            let b = if let DTypes::I32(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out: Vec<i32> = vec![0; len];
-            a.par_iter()
-                .zip(b.par_iter())
-                .zip(out.par_iter_mut())
-                .for_each(|((a, b), dst)| {
-                    *dst = a / b;
-                });
-            DTypes::I32(out)
+            let b = extract_data!(b, DTypes::I32);
+            DTypes::I32(par!(a, b, i32))
         },
         DTypes::U8(a) => {
-            let b = if let DTypes::U8(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out: Vec<u8> = vec![0; len];
-            a.par_iter()
-                .zip(b.par_iter())
-                .zip(out.par_iter_mut())
-                .for_each(|((a, b), dst)| {
-                    *dst = a / b;
-                });
-            DTypes::U8(out)
+            let b = extract_data!(b, DTypes::U8);
+            DTypes::U8(par!(a, b, u8))
         },
         DTypes::U16(a) => {
-            let b = if let DTypes::U16(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out: Vec<u16> = vec![0; len];
-            a.par_iter()
-                .zip(b.par_iter())
-                .zip(out.par_iter_mut())
-                .for_each(|((a, b), dst)| {
-                    *dst = a / b;
-                });
-            DTypes::U16(out)
+            let b = extract_data!(b, DTypes::U16);
+            DTypes::U16(par!(a, b, u16))
         },
         DTypes::U32(a) => {
-            let b = if let DTypes::U32(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out: Vec<u32> = vec![0; len];
-            a.par_iter()
-                .zip(b.par_iter())
-                .zip(out.par_iter_mut())
-                .for_each(|((a, b), dst)| {
-                    *dst = a / b;
-                });
-            DTypes::U32(out)
+            let b = extract_data!(b, DTypes::U32);
+            DTypes::U32(par!(a, b, u32))
         },
         DTypes::F32(a) => {
-            let b = if let DTypes::F32(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out = vec![0.; len];
-            a.par_chunks(4)
-                .zip(b.par_chunks(4))
-                .zip(out.par_chunks_mut(4))
-                .for_each(|((a, b), dst)| unsafe {
-                    let a = v128_load(a.as_ptr() as *const v128);
-                    let b = v128_load(b.as_ptr() as *const v128);
-                    let result = f32x4_div(a, b);
-                    v128_store(dst.as_mut_ptr() as *mut v128, result);
-                });
-            DTypes::F32(out)
+            let b = extract_data!(b, DTypes::F32);
+            DTypes::F32(simd_par!(a, b, f32, f32x4_div))
         },
         DTypes::F64(a) => {
-            let b = if let DTypes::F64(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-            let mut out = vec![0.; len];
-            a.par_chunks(4)
-                .zip(b.par_chunks(2))
-                .zip(out.par_chunks_mut(2))
-                .for_each(|((a, b), dst)| unsafe {
-                    let a = v128_load(a.as_ptr() as *const v128);
-                    let b = v128_load(b.as_ptr() as *const v128);
-                    let result = f64x2_div(a, b);
-                    v128_store(dst.as_mut_ptr() as *mut v128, result);
-                });
-            DTypes::F64(out)
+            let b = extract_data!(b, DTypes::F64);
+            DTypes::F64(simd_par!(a, b, f64, f64x2_div))
         },
     };
 
     Tensor::new(out_data, out_shape)
 }
 
-fn handle_broadcast(a: &Tensor, b: &Tensor, a_broadcast_dim: &Vec<usize>, b_broadcast_dim: &Vec<usize>, out_shape: Vec<usize>) -> Tensor {
-    let mut output = Tensor::new_empty();
-    output.set_shape(out_shape);
 
-    let a_stride = a.get_stride();
-    let a_dim = a_stride.len();
-    let b_stride = b.get_stride();
-    let b_dim = b_stride.len();
-    let out_len = output.get_length();
-    let out_stride = output.get_stride();
+broadcast!(div, /);
 
-    let out_data = match &a.get_data() {
-        DTypes::I8(a) => {
-            let b = if let DTypes::I8(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::I8(out_data)
-        }
-        DTypes::I16(a) => {
-            let b = if let DTypes::I16(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::I16(out_data)
-        },
-        DTypes::I32(a) => {
-            let b = if let DTypes::I32(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::I32(out_data)
-        },
-        DTypes::U8(a) => {
-            let b = if let DTypes::U8(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::U8(out_data)
-        },
-        DTypes::U16(a) => {
-            let b = if let DTypes::U16(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::U16(out_data)
-        },
-        DTypes::U32(a) => {
-            let b = if let DTypes::U32(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::U32(out_data)
-        },
-        DTypes::F32(a) => {
-            let b = if let DTypes::F32(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0.; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::F32(out_data)
-        },
-        DTypes::F64(a) => {
-            let b = if let DTypes::F64(data) = b.get_data() {
-                data
-            } else {
-                panic!("Two tensors' data types do not match!")
-            };
-
-            let mut out_data = vec![0.; out_len];
-            let out_data_mutex = Mutex::new(&mut out_data);
-
-            (0..out_len).into_par_iter().for_each(|i| {
-                let out_loc = idx_to_loc(i, &out_stride);
-
-                let a_loc = get_broadcast_loc(&out_loc, a_dim, &*a_broadcast_dim);
-                let a_idx = loc_to_idx(&a_loc, &a_stride);
-
-                let b_loc = get_broadcast_loc(&out_loc, b_dim, &*b_broadcast_dim);
-                let b_idx = loc_to_idx(&b_loc, &b_stride);
-
-                let value = a[a_idx] / b[b_idx];
-                let mut out_data = out_data_mutex.lock().unwrap();
-                out_data[i] = value;
-            });
-
-            DTypes::F64(out_data)
-        },
-    };
-
-    output.set_data(out_data);
-
-    output
-}
+handle_binaryop!(div, /);
--- wasm/src/rs/src/layers/binaryop/mod.rs
+++ wasm/src/rs/src/layers/binaryop/mod.rs
@@ -3,6 +3,205 @@ mod sub;
 mod mul;
 mod div;
 
+macro_rules! extract_data {
+    ($tensor: expr, $vari: path) => {
+        if let $vari(data) = $tensor.get_data() {
+            data
+        } else {
+            panic!("The data types of tensors do not match!")
+        }
+    };
+}
+
+macro_rules! par {
+    ($a: expr, $b: expr, $typ: ty) => {
+        {
+            let mut out = vec![0 as $typ; $a.len()];
+            $a.par_iter()
+                .zip($b.par_iter())
+                .zip(out.par_iter_mut())
+                .for_each(|((a, b), dst)| {
+                    *dst = a * b;
+                });
+            out
+        }
+    };
+}
+
+macro_rules! simd_par {
+    ($a: expr, $b: expr, $typ: ty, $simd: ident) => {
+        {
+            let chunks_num: usize = 16 / mem::size_of::<$typ>();
+            let mut out = vec![0 as $typ; $a.len()];
+            $a.par_chunks(chunks_num)
+                .zip($b.par_chunks(chunks_num))
+                .zip(out.par_chunks_mut(chunks_num))
+                .for_each(|((a, b), dst)| unsafe {
+                    let a = v128_load(a.as_ptr() as *const v128);
+                    let b = v128_load(b.as_ptr() as *const v128);
+                    let result = $simd(a, b);
+                    v128_store(dst.as_mut_ptr() as *mut v128, result);
+                });
+            out
+        }
+    };
+}
+
+macro_rules! broadcast_inner {
+    (
+        $a: expr, $b: expr, $typ: ty, $op: tt,
+        $a_stride: expr, $a_dim: expr, 
+        $b_stride: expr, $b_dim: expr, 
+        $out_stride: expr, $out_len: expr,
+        $a_broadcast_dim: expr, $b_broadcast_dim: expr
+    ) => {
+        {
+            let mut out_data = vec![0 as $typ; $out_len];
+            let out_data_mutex = Mutex::new(&mut out_data);
+    
+            (0..$out_len).into_par_iter().for_each(|i| {
+                let out_loc = idx_to_loc(i, &$out_stride);
+    
+                let a_loc = get_broadcast_loc(&out_loc, $a_dim, &*$a_broadcast_dim);
+                let a_idx = loc_to_idx(&a_loc, &$a_stride);
+    
+                let b_loc = get_broadcast_loc(&out_loc, $b_dim, &*$b_broadcast_dim);
+                let b_idx = loc_to_idx(&b_loc, &$b_stride);
+    
+                let value = $a[a_idx] $op $b[b_idx];
+                let mut out_data = out_data_mutex.lock().unwrap();
+                out_data[i] = value;
+            });
+    
+            out_data
+        }
+    };
+}
+
+macro_rules! broadcast {
+    ($name: ident, $op: tt) => {
+        paste! {
+            fn [< $name _broadcast >](a: &Tensor, b: &Tensor, a_broadcast_dim: &Vec<usize>, b_broadcast_dim: &Vec<usize>, out_shape: Vec<usize>) -> Tensor {
+                let mut output = Tensor::new_empty();
+                output.set_shape(out_shape);
+            
+                let a_stride = a.get_stride();
+                let a_dim = a_stride.len();
+                let b_stride = b.get_stride();
+                let b_dim = b_stride.len();
+                let out_len = output.get_length();
+                let out_stride = output.get_stride();
+            
+                let out_data = match &a.get_data() {
+                    DTypes::I8(a) => {
+                        let b = extract_data!(b, DTypes::I8);
+                        DTypes::I8(broadcast_inner!(
+                            a, b, i8, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    }
+                    DTypes::I16(a) => {
+                        let b = extract_data!(b, DTypes::I16);
+                        DTypes::I16(broadcast_inner!(
+                            a, b, i16, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    },
+                    DTypes::I32(a) => {
+                        let b = extract_data!(b, DTypes::I32);
+                        DTypes::I32(broadcast_inner!(
+                            a, b, i32, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    },
+                    DTypes::U8(a) => {
+                        let b = extract_data!(b, DTypes::U8);
+                        DTypes::U8(broadcast_inner!(
+                            a, b, u8, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    },
+                    DTypes::U16(a) => {
+                        let b = extract_data!(b, DTypes::U16);
+                        DTypes::U16(broadcast_inner!(
+                            a, b, u16, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    },
+                    DTypes::U32(a) => {
+                        let b = extract_data!(b, DTypes::U32);
+                        DTypes::U32(broadcast_inner!(
+                            a, b, u32, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    },
+                    DTypes::F32(a) => {
+                        let b = extract_data!(b, DTypes::F32);
+                        DTypes::F32(broadcast_inner!(
+                            a, b, f32, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    },
+                    DTypes::F64(a) => {
+                        let b = extract_data!(b, DTypes::F64);
+                        DTypes::F64(broadcast_inner!(
+                            a, b, f64, $op,
+                            a_stride, a_dim, 
+                            b_stride, b_dim, 
+                            out_stride, out_len, 
+                            a_broadcast_dim, b_broadcast_dim
+                        ))
+                    },
+                };
+            
+                output.set_data(out_data);
+            
+                output
+            }
+        }
+    };
+}
+
+macro_rules! handle_binaryop {
+    ($name: expr, $op: tt) => {
+        paste! {
+            #[wasm_bindgen(js_name = [<handle $name:camel>])]
+            pub fn [<handle_ $name>](a: &Tensor, b: &Tensor) -> Tensor {
+                let (a_shape, b_shape) = (a.get_shape(), b.get_shape());
+                let out_shape = get_broadcast_shape(&a_shape, &b_shape);
+                let (a_broadcast_dim, b_broadcast_dim) = (get_broadcast_dims(&a_shape, &out_shape), get_broadcast_dims(&b_shape, &out_shape));
+            
+                if a_broadcast_dim.len() + b_broadcast_dim.len() == 0 {
+                    [<$name _same_shape>](a, b, out_shape)
+                } else {
+                    [<$name _broadcast>](a, b, &a_broadcast_dim, &b_broadcast_dim, out_shape)
+                }
+            }
+        }
+    };
+}
+
 fn idx_to_loc(index: usize, stride: &Vec<usize>) -> Vec<usize> {
     let mut loc = vec![0; stride.len()];
     let mut index = index;
@@ -73,3 +272,10 @@ fn get_broadcast_loc(out_loc: &Vec<usize>, dim: usize, broadcast_dim: &Vec<usize
 
     loc
 }
+
+pub(crate) use extract_data;
+pub(crate) use par;
+pub(crate) use simd_par;
+pub(crate) use broadcast_inner;
+pub(crate) use broadcast;
+pub(crate) use handle_binaryop;
