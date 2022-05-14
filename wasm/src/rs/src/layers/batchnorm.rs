21b613d64004c82415ad7ed00ec4f57c03f5455d
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-14 22:46:51 +0800

    refactor(wasm): refactor batchnorm layer by macro.

--- wasm/src/rs/src/layers/batchnorm.rs
+++ wasm/src/rs/src/layers/batchnorm.rs
@@ -2,6 +2,27 @@ use wasm_bindgen::prelude::*;
 
 use crate::{DTypes, Tensor};
 
+use super::extract_data;
+
+macro_rules! batchnorm {
+    ($input: expr, $scale: expr, $bias: expr, $mean: expr, $variance: expr, $dtype: path) => {
+        {
+            let scale = extract_data!(&$scale, $dtype);
+            let bias = extract_data!(&$bias, $dtype);
+            let mean = extract_data!(&$mean, $dtype);
+            let variance = extract_data!(&$variance, $dtype);
+    
+            let mut out = Vec::with_capacity($input.len());
+            for (index, val) in $input.iter().enumerate() {
+                out[index] =
+                    ((val - mean[index]) / variance[index].sqrt()) * scale[index] + bias[index];
+            }
+
+            out
+        }
+    };
+}
+
 #[wasm_bindgen(js_name = handleBatchNorm)]
 pub fn handle_batchnorm(
     input: &Tensor,
@@ -12,66 +33,8 @@ pub fn handle_batchnorm(
 ) -> Tensor {
     let out_shape = input.get_shape();
     let out_data = match &input.get_data() {
-        DTypes::F32(arr) => {
-            let scale = if let DTypes::F32(arr) = &scale.get_data() {
-                arr
-            } else {
-                panic!("Scale's type is not float32!");
-            };
-            let bias = if let DTypes::F32(arr) = &bias.get_data() {
-                arr
-            } else {
-                panic!("Bias's type is not float32!");
-            };
-            let mean = if let DTypes::F32(arr) = &mean.get_data() {
-                arr
-            } else {
-                panic!("Mean's type is not float32!");
-            };
-            let variance = if let DTypes::F32(arr) = &variance.get_data() {
-                arr
-            } else {
-                panic!("Variance's type is not float32!");
-            };
-
-            let mut out = Vec::with_capacity(input.get_length());
-            for (index, val) in arr.iter().enumerate() {
-                out[index] =
-                    ((val - mean[index]) / variance[index].sqrt()) * scale[index] + bias[index];
-            }
-
-            DTypes::F32(out)
-        }
-        DTypes::F64(arr) => {
-            let scale = if let DTypes::F64(arr) = &scale.get_data() {
-                arr
-            } else {
-                panic!("Scale's type is not float32!");
-            };
-            let bias = if let DTypes::F64(arr) = &bias.get_data() {
-                arr
-            } else {
-                panic!("Bias's type is not float32!");
-            };
-            let mean = if let DTypes::F64(arr) = &mean.get_data() {
-                arr
-            } else {
-                panic!("Mean's type is not float32!");
-            };
-            let variance = if let DTypes::F64(arr) = &variance.get_data() {
-                arr
-            } else {
-                panic!("Variance's type is not float32!");
-            };
-
-            let mut out = Vec::with_capacity(input.get_length());
-            for (index, val) in arr.iter().enumerate() {
-                out[index] =
-                    ((val - mean[index]) / variance[index].sqrt()) * scale[index] + bias[index];
-            }
-
-            DTypes::F64(out)
-        }
+        DTypes::F32(arr) => DTypes::F32(batchnorm!(arr, scale, bias, mean, variance, DTypes::F32)),
+        DTypes::F64(arr) => DTypes::F64(batchnorm!(arr, scale, bias, mean, variance, DTypes::F64)),
         _ => panic!("Batch normalization not support on target data type!"),
     };
 
--- wasm/src/rs/src/layers/mod.rs
+++ wasm/src/rs/src/layers/mod.rs
@@ -9,3 +9,15 @@ pub mod pooling;
 pub mod relu;
 pub mod reshape;
 pub mod shape;
+
+macro_rules! extract_data {
+    ($tensor: expr, $dtype: path) => {
+        if let $dtype(data) = $tensor.get_data() {
+            data
+        } else {
+            panic!("The data types of tensors do not match!")
+        }
+    };
+}
+
+pub(crate) use extract_data;
