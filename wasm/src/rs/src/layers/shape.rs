618e5fc9ff4bd57424bb135bc7da3dada33b6479
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-14 21:41:30 +0800

    refactor(wasm): refactor shape layer by macro.

--- wasm/src/rs/src/layers/shape.rs
+++ wasm/src/rs/src/layers/shape.rs
@@ -2,43 +2,27 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 use crate::{DTypes, Tensor};
 
+macro_rules! cast {
+    ($shape: expr, $typ: ty) => {
+        {
+            $shape.iter().map(|&x| x as $typ).collect()
+        }
+    };
+}
+
 #[wasm_bindgen(js_name = handleShape)]
 pub fn handle_shape(input: &Tensor) -> Tensor {
     let shape = input.get_shape();
     let out_shape = vec![shape.len()];
     let out_data = match &input.get_data() {
-        DTypes::I8(_) => {
-            let out = shape.iter().map(|&x| x as i8).collect();
-            DTypes::I8(out)
-        }
-        DTypes::I16(_) => {
-            let out = shape.iter().map(|&x| x as i16).collect();
-            DTypes::I16(out)
-        }
-        DTypes::I32(_) => {
-            let out = shape.iter().map(|&x| x as i32).collect();
-            DTypes::I32(out)
-        }
-        DTypes::U8(_) => {
-            let out = shape.iter().map(|&x| x as u8).collect();
-            DTypes::U8(out)
-        }
-        DTypes::U16(_) => {
-            let out = shape.iter().map(|&x| x as u16).collect();
-            DTypes::U16(out)
-        }
-        DTypes::U32(_) => {
-            let out = shape.iter().map(|&x| x as u32).collect();
-            DTypes::U32(out)
-        }
-        DTypes::F32(_) => {
-            let out = shape.iter().map(|&x| x as f32).collect();
-            DTypes::F32(out)
-        }
-        DTypes::F64(_) => {
-            let out = shape.iter().map(|&x| x as f64).collect();
-            DTypes::F64(out)
-        }
+        DTypes::I8(_) => DTypes::I8(cast!(shape, i8)),
+        DTypes::I16(_) => DTypes::I16(cast!(shape, i16)),
+        DTypes::I32(_) => DTypes::I32(cast!(shape, i32)),
+        DTypes::U8(_) => DTypes::U8(cast!(shape, u8)),
+        DTypes::U16(_) => DTypes::U16(cast!(shape, u16)),
+        DTypes::U32(_) => DTypes::U32(cast!(shape, u32)),
+        DTypes::F32(_) => DTypes::F32(cast!(shape, f32)),
+        DTypes::F64(_) => DTypes::F64(cast!(shape, f64)),
     };
 
     Tensor::new(out_data, out_shape)
