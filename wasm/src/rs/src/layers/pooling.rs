66ec2438498e753995d7b0391b5c8794c8fef4dc
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-17 21:48:52 +0800

    refactor(wasm): refactor pooling layer with macro.

--- wasm/src/rs/src/layers/pooling.rs
+++ wasm/src/rs/src/layers/pooling.rs
@@ -1,6 +1,48 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::{DTypes, Tensor};
+use crate::{DTypes, Tensor, layers::padding::handle_padding};
+
+macro_rules! maxpool {
+    (
+        $arr: expr, $in_shape: expr, $out_shape: expr, $len: expr,
+        $kernel_height: expr, $kernel_width: expr, 
+        $stride_y: expr, $stride_x: expr, $typ: ty
+    ) => {
+        {
+            let in_channel_size = $in_shape[2] * $in_shape[3];
+            let row_stride = $in_shape[3] - $kernel_width;
+
+            let mut out_idx = 0;
+            let mut out_data = vec![0.; $len];
+            for _n in 0..$out_shape[0] {
+                for c in 0..$out_shape[1] {
+                    for y in 0..$out_shape[2] {
+                        for x in 0..$out_shape[3] {
+                            let in_start_y = y * $stride_y;
+                            let in_start_x = x * $stride_x;
+
+                            let mut offset = c * in_channel_size + in_start_y * $in_shape[3] + in_start_x;
+                            let mut max = $arr[offset];
+                            for _ky in 0..$kernel_height {
+                                for _kx in 0..$kernel_width {
+                                    let val = $arr[offset];
+                                    max = max.max(val);
+
+                                    offset += 1;
+                                }
+                                offset += row_stride;
+                            }
+                            out_data[out_idx] = max as $typ;
+                            out_idx += 1;
+                        }
+                    }
+                }
+            }
+
+            out_data
+        }
+    };
+}
 
 #[wasm_bindgen(js_name = handleMaxPool2D)]
 pub fn handle_maxpool_2d(
@@ -14,115 +56,74 @@ pub fn handle_maxpool_2d(
     stride_y: usize,
     stride_x: usize,
 ) -> Tensor {
+    let padding = if pad_top != 0 || pad_left != 0 || pad_bottom != 0 || pad_right != 0 {
+        handle_padding(input, pad_top, pad_left, pad_bottom, pad_right)
+    } else {
+        input.clone()
+    };
+
     let mut output = Tensor::new_empty();
 
-    let in_shape = input.get_shape();
-    let max_y = in_shape[2] + pad_top + pad_bottom - kernel_height;
-    let max_x = in_shape[3] + pad_left + pad_right - kernel_width;
-    let out_height = max_y / stride_y + 1;
-    let out_width = max_x / stride_x + 1;
-    let out_shape = vec![in_shape[0], in_shape[1], out_height, out_width];
-    output.set_shape(out_shape);
-
-    let in_channel_size = in_shape[2] * in_shape[3];
-    let in_size = in_shape[1] * in_channel_size;
+    let in_shape = padding.get_shape();
+    let out_shape = vec![
+        in_shape[0], 
+        in_shape[1], 
+        (in_shape[2] - kernel_height) / stride_y + 1,
+        (in_shape[3] - kernel_width) / stride_x + 1,    
+    ];
+    output.set_shape(out_shape.clone());
 
     let out_data = match &input.get_data() {
-        DTypes::F32(arr) => {
-            let mut out_idx = 0;
-            let mut out_data = vec![0.; output.get_length()];
-            for n in 0..in_shape[0] {
-                for c in 0..in_shape[1] {
-                    for y in (-(pad_top as isize)..=(max_y - pad_top) as isize).step_by(stride_y) {
-                        for x in
-                            (-(pad_left as isize)..=(max_x - pad_left) as isize).step_by(stride_x)
-                        {
-                            let max_idx = n as isize * in_size as isize
-                                + c as isize * in_channel_size as isize
-                                + y * in_shape[3] as isize
-                                + x;
-                            let mut max_val = arr[max_idx as usize];
-                            for ky in 0..kernel_height {
-                                for kx in 0..kernel_width {
-                                    let cy = y + ky as isize;
-                                    let cx = x + kx as isize;
-                                    let val = if cy >= 0
-                                        && cy < in_shape[2] as isize
-                                        && cx >= 0
-                                        && cx < in_shape[3] as isize
-                                    {
-                                        let cur_idx = n * in_size
-                                            + c * in_channel_size
-                                            + cy as usize * in_shape[3]
-                                            + cx as usize;
-                                        arr[cur_idx]
-                                    } else {
-                                        0.
-                                    };
-
-                                    if val > max_val {
-                                        max_val = val;
-                                    }
-                                }
-                            }
-                            out_data[out_idx] = max_val;
-                            out_idx += 1;
-                        }
-                    }
-                }
-            }
-            DTypes::F32(out_data)
-        }
-        DTypes::F64(arr) => {
-            let mut out_idx = 0;
-            let mut out_data = vec![0.; output.get_length()];
-            for n in 0..in_shape[0] {
-                for c in 0..in_shape[1] {
-                    for y in (-(pad_top as isize)..=(max_y - pad_top) as isize).step_by(stride_y) {
-                        for x in
-                            (-(pad_left as isize)..=(max_x - pad_left) as isize).step_by(stride_x)
-                        {
-                            let max_idx = n as isize * in_size as isize
-                                + c as isize * in_channel_size as isize
-                                + y * in_shape[3] as isize
-                                + x;
-                            let mut max_val = arr[max_idx as usize];
-                            for ky in 0..kernel_height {
-                                for kx in 0..kernel_width {
-                                    let cy = y + ky as isize;
-                                    let cx = x + kx as isize;
-                                    let val = if cy >= 0
-                                        && cy < in_shape[2] as isize
-                                        && cx >= 0
-                                        && cx < in_shape[3] as isize
-                                    {
-                                        let cur_idx = n * in_size
-                                            + c * in_channel_size
-                                            + cy as usize * in_shape[3]
-                                            + cx as usize;
-                                        arr[cur_idx]
-                                    } else {
-                                        0.
-                                    };
-
-                                    if val > max_val {
-                                        max_val = val;
-                                    }
-                                }
-                            }
-                            out_data[out_idx] = max_val;
-                            out_idx += 1;
-                        }
-                    }
-                }
-            }
-            DTypes::F64(out_data)
-        }
+        DTypes::F32(arr) => DTypes::F32(maxpool!(arr, in_shape, out_shape, output.get_length(), kernel_height, kernel_width, stride_y, stride_x, f32)),
+        DTypes::F64(arr) => DTypes::F64(maxpool!(arr, in_shape, out_shape, output.get_length(), kernel_height, kernel_width, stride_y, stride_x, f64)),
         _ => panic!("Max pooling does not support these data types."),
     };
     output.set_data(out_data);
 
     output
+}
+
+macro_rules! avgpool {
+    (
+        $arr: expr, $in_shape: expr, $out_shape: expr, $len: expr,
+        $kernel_height: expr, $kernel_width: expr, 
+        $stride_y: expr, $stride_x: expr, $typ: ty
+    ) => {
+        {
+            let in_channel_size = $in_shape[2] * $in_shape[3];
+            let kernel_size = $kernel_height * $kernel_width;
+            let row_stride = $in_shape[3] - $kernel_width;
+
+            let mut out_idx = 0;
+            let mut out_data = vec![0.; $len];
+            for _n in 0..$out_shape[0] {
+                for c in 0..$out_shape[1] {
+                    for y in 0..$out_shape[2] {
+                        for x in 0..$out_shape[3] {
+                            let in_start_y = y * $stride_y;
+                            let in_start_x = x * $stride_x;
+
+                            let mut sum = 0.;
+                            let mut offset = c * in_channel_size + in_start_y * $in_shape[3] + in_start_x;
+                            for _ky in 0..$kernel_height {
+                                for _kx in 0..$kernel_width {
+                                    let val = $arr[offset];
+
+                                    sum += val;
+                                    offset += 1;
+                                }
+                                offset += row_stride;
+                            }
+                            out_data[out_idx] = sum / kernel_size as $typ;
+                            out_idx += 1;
+                        }
+                    }
+                }
+            }
+
+            out_data
+        }
+    };
 }
 
 #[wasm_bindgen(js_name = handleAvgPool2D)]
@@ -137,99 +138,26 @@ pub fn handle_avgpool_2d(
     stride_y: usize,
     stride_x: usize,
 ) -> Tensor {
+    let padding = if pad_top != 0 || pad_left != 0 || pad_bottom != 0 || pad_right != 0 {
+        handle_padding(input, pad_top, pad_left, pad_bottom, pad_right)
+    } else {
+        input.clone()
+    };
+
     let mut output = Tensor::new_empty();
 
-    let in_shape = input.get_shape();
-    let max_y = in_shape[2] + pad_top + pad_bottom - kernel_height;
-    let max_x = in_shape[3] + pad_left + pad_right - kernel_width;
-    let out_height = max_y / stride_y + 1;
-    let out_width = max_x / stride_x + 1;
-    let out_shape = vec![in_shape[0], in_shape[1], out_height, out_width];
-    output.set_shape(out_shape);
+    let in_shape = padding.get_shape();
+    let out_shape = vec![
+        in_shape[0], 
+        in_shape[1], 
+        (in_shape[2] - kernel_height) / stride_y + 1,
+        (in_shape[3] - kernel_width) / stride_x + 1,    
+    ];
+    output.set_shape(out_shape.clone());
 
-    let in_channel_size = in_shape[2] * in_shape[3];
-    let in_size = in_shape[1] * in_channel_size;
-    let kernel_size = kernel_height * kernel_width;
-
-    let out_data = match &input.get_data() {
-        DTypes::F32(arr) => {
-            let mut out_idx = 0;
-            let mut out_data = vec![0.; output.get_length()];
-            for n in 0..in_shape[0] {
-                for c in 0..in_shape[1] {
-                    for y in (-(pad_top as isize)..=(max_y - pad_top) as isize).step_by(stride_y) {
-                        for x in
-                            (-(pad_left as isize)..=(max_x - pad_left) as isize).step_by(stride_x)
-                        {
-                            let mut sum = 0.;
-                            for ky in 0..kernel_height {
-                                for kx in 0..kernel_width {
-                                    let cy = y + ky as isize;
-                                    let cx = x + kx as isize;
-                                    let val = if cy >= 0
-                                        && cy < in_shape[2] as isize
-                                        && cx >= 0
-                                        && cx < in_shape[3] as isize
-                                    {
-                                        let cur_idx = n * in_size
-                                            + c * in_channel_size
-                                            + cy as usize * in_shape[3]
-                                            + cx as usize;
-                                        arr[cur_idx]
-                                    } else {
-                                        0.
-                                    };
-
-                                    sum += val;
-                                }
-                            }
-                            out_data[out_idx] = sum / kernel_size as f32;
-                            out_idx += 1;
-                        }
-                    }
-                }
-            }
-            DTypes::F32(out_data)
-        }
-        DTypes::F64(arr) => {
-            let mut out_idx = 0;
-            let mut out_data = vec![0.; output.get_length()];
-            for n in 0..in_shape[0] {
-                for c in 0..in_shape[1] {
-                    for y in (-(pad_top as isize)..=(max_y - pad_top) as isize).step_by(stride_y) {
-                        for x in
-                            (-(pad_left as isize)..=(max_x - pad_left) as isize).step_by(stride_x)
-                        {
-                            let mut sum = 0.;
-                            for ky in 0..kernel_height {
-                                for kx in 0..kernel_width {
-                                    let cy = y + ky as isize;
-                                    let cx = x + kx as isize;
-                                    let val = if cy >= 0
-                                        && cy < in_shape[2] as isize
-                                        && cx >= 0
-                                        && cx < in_shape[3] as isize
-                                    {
-                                        let cur_idx = n * in_size
-                                            + c * in_channel_size
-                                            + cy as usize * in_shape[3]
-                                            + cx as usize;
-                                        arr[cur_idx]
-                                    } else {
-                                        0.
-                                    };
-
-                                    sum += val;
-                                }
-                            }
-                            out_data[out_idx] = sum / kernel_size as f64;
-                            out_idx += 1;
-                        }
-                    }
-                }
-            }
-            DTypes::F64(out_data)
-        }
+    let out_data = match &padding.get_data() {
+        DTypes::F32(arr) => DTypes::F32(avgpool!(arr, in_shape, out_shape, output.get_length(), kernel_height, kernel_width, stride_y, stride_x, f32)),
+        DTypes::F64(arr) => DTypes::F64(avgpool!(arr, in_shape, out_shape, output.get_length(), kernel_height, kernel_width, stride_y, stride_x, f64)),
         _ => panic!("Max pooling does not support these data types."),
     };
     output.set_data(out_data);
