2885656dc7f25194e77296cc21218382d37f7fe0
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-20 20:55:53 +0800

    build(bench): remove circular dependency warnings.

--- bench/webpack.config.js
+++ bench/webpack.config.js
@@ -3,6 +3,7 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = [
   {
+    // For shorter compilation time
     mode: "development",
     devtool: "source-map",
     entry: path.join(__dirname, "index.ts"),
@@ -28,5 +29,8 @@ module.exports = [
       filename: "index.js",
       path: path.join(__dirname, "./dist"),
     },
+    ignoreWarnings: [{
+      message: /Circular dependency/
+    }],
   },
 ];
